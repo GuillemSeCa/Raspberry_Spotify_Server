ff508564c0219c9e501056e882dfccc8c3e9037d
Guillem Serra Cazorla <44067226+GuillemSeCa@users.noreply.github.com>
2022-01-01 19:13:29 +0100

    Update afegircron.sh

--- CRON/afegircron.sh
+++ CRON/afegircron.sh
@@ -14,7 +14,7 @@ crontab -l > $HOME/mycron.txt
 #echo new cron into cron file
 echo '\n$cron' >> $HOME/mycron.txt
 #install new cron file
-sudo crontab $HOME/mycron.txt
+sudo crontab $HOME/mycron.txt -u www-data
 
 echo -e '	<!DOCTYPE html><html>
 		<body>
