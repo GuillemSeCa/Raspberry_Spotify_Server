fe4a51da8c2433a23b14da94436e1223326eba39
Guillem Serra Cazorla <44067226+GuillemSeCa@users.noreply.github.com>
2022-01-01 18:04:59 +0100

    Update afegircron.sh

--- CRON/afegircron.sh
+++ CRON/afegircron.sh
@@ -9,12 +9,12 @@ cron=`echo $aux1 | awk -F\& '{print $1}'`
 
 
 #write out current crontab
-crontab -l > mycron.txt
+crontab -l > ./mycron.txt
 #echo new cron into cron file
-echo "$cron" >> mycron.txt
+echo "$cron" >> ./mycron.txt
 #install new cron file
-crontab mycron.txt
-rm mycron.txt
+crontab ./mycron.txt
+rm ./mycron.txt
 
 
 echo -e "	<!DOCTYPE html><html>
