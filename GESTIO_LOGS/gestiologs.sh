3916212b9256b269525881d04864de669aad71b9
Pol Piñol Castuera <67922619+PolPinol@users.noreply.github.com>
2022-01-07 23:34:39 +0100

    Update gestiologs.sh

--- GESTIO_LOGS/gestiologs.sh
+++ GESTIO_LOGS/gestiologs.sh
@@ -6,9 +6,13 @@ echo
 
 echo -e "	<!DOCTYPE html><html>
 		<body>
-			<h1>GESTIO DE LOGS</h1>
-
-            <form action="../webapp.sh">
+			<h1>GESTIO DE LOGS</h1>"
+			
+		while IFS="" read -r -a line;
+		do
+			echo "<p>$line</p>";
+		done <<< "$(sudo cat HOME/logs.txt)"
+echo -e "   <form action="../webapp.sh">
                 <input type="submit" value="Return">
             </form>
 		</body>
