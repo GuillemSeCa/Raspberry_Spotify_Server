d100cc19b28e94ebb6300b22ba9f1a4f5ce2aeaa
Pol Piñol Castuera <67922619+PolPinol@users.noreply.github.com>
2022-01-08 12:21:43 +0100

    Update filter.sh

--- FILTRAT_PAQUETS/filter.sh
+++ FILTRAT_PAQUETS/filter.sh
@@ -6,9 +6,10 @@ echo -e "<!DOCTYPE html><html><body>"
 
 aux1=`echo $QUERY_STRING | awk -F= '{print $2}'`
 prot=`echo $aux1 | awk -F\& '{print $1}'`
-src=`echo $QUERY_STRING | awk -F\= '{print $3}'`
-dest=`echo $QUERY_STRING | awk -F\= '{print $3}'`
-port=`echo $QUERY_STRING | awk -F\= '{print $3}'`
+aux2=`echo $QUERY_STRING | awk -F\= '{print $3}'`
+src=`echo $aux2 | awk -F\& '{print $3}'`
+dest=`echo $QUERY_STRING | awk -F\= '{print $4}'`
+port=`echo $QUERY_STRING | awk -F\= '{print $5}'`
 
 echo -e "	<h1>IPTABLE added</h1>
   <p>$prot</p>
