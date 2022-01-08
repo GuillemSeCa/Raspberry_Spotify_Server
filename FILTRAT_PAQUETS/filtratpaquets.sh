4b5d90a0899cacfbbb6e5994ab401134c773aad0
Pol Piñol Castuera <67922619+PolPinol@users.noreply.github.com>
2022-01-08 11:55:53 +0100

    Update filtratpaquets.sh

--- FILTRAT_PAQUETS/filtratpaquets.sh
+++ FILTRAT_PAQUETS/filtratpaquets.sh
@@ -7,23 +7,12 @@ echo
 echo -e "	<!DOCTYPE html><html>
 		<body>
 			<h1>FILTRAT PAQUETS</h1>
-
-            <form action="" method="get" ENCTYPE="text/plain">
+	    <h2>Veure tots els filtrats</h2>
+	    
+            <form action="filter.sh" method="get" ENCTYPE="text/plain">
 			Name: <input type="text" name="name" size="20">
-			<input type="submit" value="Filtrar">
-            </form>
-
-            <form action="" method="get" ENCTYPE="text/plain">
 			Source IP: <input type="text" name="name" size="20">
-			<input type="submit" value="Filtrar">
-            </form>
-
-            <form action="" method="get" ENCTYPE="text/plain">
 			Destiny IP: <input type="text" name="name" size="20">
-			<input type="submit" value="Filtrar">
-            </form>
-
-            <form action="" method="get" ENCTYPE="text/plain">
 			Port: <input type="text" name="name" size="20">
 			<input type="submit" value="Filtrar">
             </form>
