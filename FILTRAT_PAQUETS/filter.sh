653567d6015f950293e7790fbb470636a6390878
Pol Piñol Castuera <67922619+PolPinol@users.noreply.github.com>
2022-01-08 13:04:12 +0100

    Update filter.sh

--- FILTRAT_PAQUETS/filter.sh
+++ FILTRAT_PAQUETS/filter.sh
@@ -6,16 +6,47 @@ echo -e "<!DOCTYPE html><html><body>"
 
 aux1=`echo $QUERY_STRING | awk -F= '{print $2}'`
 prot=`echo $aux1 | awk -F\& '{print $1}'`
-aux2=`echo $QUERY_STRING | awk -F\= '{print $3}'`
-src=`echo $aux2 | awk -F\& '{print $3}'`
-dest=`echo $QUERY_STRING | awk -F\= '{print $4}'`
+aux2=`echo $QUERY_STRING  | awk -F\& '{print $2}'`
+src=`echo $aux2  | awk -F\= '{print $2}'`
+aux3=`echo $QUERY_STRING | awk -F\& '{print $3}'`
+dest=`echo $aux3 | awk -F\= '{print $2}'`
 port=`echo $QUERY_STRING | awk -F\= '{print $5}'`
 
+if [ -z "$prot" ];
+then
+	protT="-p $prot"
+else
+	protT=""
+fi
+
+if [ -z "$src" ];
+then
+	srcT="-s $src"
+else
+	srcT=""
+fi
+
+if [ -z "$dest" ];
+then
+	destT="-d $dest"
+else
+	destT=""
+fi
+
+if [ -z "$port" ];
+then
+	portT="--dport $port"
+else
+	portT=""
+fi
+
+iptable="$(protT) $(srcT) $(destT) $(portT)"
+
 echo -e "	<h1>IPTABLE added</h1>
-  <p>$prot</p>
-  <p>$src</p>
-  <p>$dest</p>
-  <p>$port</p>
+  <p>Protocol: $prot</p>
+  <p>Source IP: $src</p>
+  <p>Destiny IP: $dest</p>
+  <p>Port: $port</p>
     <form action="../webapp.sh">
 					<input type="submit" value="Return">
 		</form>"
